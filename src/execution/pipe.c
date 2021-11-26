0f1fab09385f7b17db505ffab7fb2a1f799e0f38
Alex375 <alexandre.josien@gmail.com>
2021-11-26 18:50:57 +0100

    added stdout reset

--- src/execution/pipe.c
+++ src/execution/pipe.c
@@ -8,11 +8,21 @@ int exec_pipe(struct ast *left, struct ast *right, struct pipeline *pipeline)
 {
     pipe(pipeline->fd);
 
+    int temp_stdout = dup(STDOUT_FILENO);
+
     pipeline->out = 1;
     int res1 = eval_ast(left, pipeline);
 
+    dup2(temp_stdout, STDOUT_FILENO);
+    close(temp_stdout);
+
+    int temp_stdin = dup(STDIN_FILENO);
+
     pipeline->out = 0;
     eval_ast(right, pipeline);
+
+    dup2(temp_stdin, STDIN_FILENO);
+    close(temp_stdin);
 
     pipeline->out = -1;
     close(pipeline->fd[0]);
