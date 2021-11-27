c9b4c2614c6b56b27f9cba0591748879dba8314b
Alex375 <alexandre.josien@gmail.com>
2021-11-27 04:39:18 +0100

    upgraded test suite

--- tests/integration_test/testsuite.py
+++ tests/integration_test/testsuite.py
@@ -5,20 +5,22 @@ from difflib import unified_diff
 from dataclasses import dataclass, field
 from contextlib import contextmanager
 import signal
+import time
 
 import termcolor
 import yaml
 from typing import List
 
 OK_TAG = f"[ {termcolor.colored('OK', 'green')} ]"
-KO_TAG = f"[ {termcolor.colored('ko', 'red')} ]"
+KO_TAG = f"[ {termcolor.colored('KO', 'red')} ]"
 
 
 @dataclass
 class TestCase:
     name: str
     input: str
-    checks: List[str] = field(default_factory=lambda: ["stdout", "stderr", "exitcode", "err_msg"])
+    checks: List[str] = field(
+        default_factory=lambda: ["stdout", "stderr", "exitcode", "err_msg"])
 
 
 def raise_timeout(signum, frame):
@@ -42,10 +44,13 @@ def timeout(time):
 def diff(expected: str, actual: str) -> str:
     expected_lines = expected.splitlines(keepends=True)
     actual_lines = actual.splitlines(keepends=True)
-    return ''.join(unified_diff(expected_lines, actual_lines, fromfile='excpected', tofile='actual'))
+    return ''.join(
+        unified_diff(expected_lines, actual_lines, fromfile='excpected',
+                     tofile='actual'))
 
 
-def perform_checks(excpected: sp.CompletedProcess, actual: sp.CompletedProcess, checks):
+def perform_checks(excpected: sp.CompletedProcess, actual: sp.CompletedProcess,
+                   checks):
     res = ""
     if "err_msg" in checks and actual.stderr == "":
         res += "Something was expected on stderr\n"
@@ -73,8 +78,13 @@ if __name__ == "__main__":
     print(f"Testing {binary_path}")
 
     with open("yaml_tests/test.yaml", "r") as file:
-        testsuite = [TestCase(**testcase) for testcase in list(yaml.safe_load(file))]
+        testsuite = [TestCase(**testcase) for testcase in
+                     list(yaml.safe_load(file))]
 
+    passed = 0
+    failed = 0
+
+    start_time = time.perf_counter()
     for testcase in testsuite:
         stdin = testcase.input
         name = testcase.name
@@ -83,14 +93,44 @@ if __name__ == "__main__":
             with timeout(1):
                 dash_proc = run_shell("dash", stdin)
                 sh_proc = run_shell(binary_path, stdin)
-                test_repport = perform_checks(dash_proc, sh_proc, testcase.checks)
+                test_repport = perform_checks(dash_proc, sh_proc,
+                                              testcase.checks)
         except Exception as err:
+            failed += 1
             if type(err) == TimeoutError and str(err) == "!!timeout!!":
                 print(f"{KO_TAG} {name}\nTest timedout")
             else:
                 print(f"{KO_TAG} {name} with arguments : {stdin}\n{err}")
         else:
             if len(test_repport) == 0:
+                passed += 1
                 print(f"{OK_TAG} {name}")
             else:
-                print(f"{KO_TAG} {name} with arguments : {stdin}\n{test_repport}")
+                failed += 1
+                print(
+                    f"{KO_TAG} {name} with arguments : {stdin}\n{test_repport}")
+    end_time = time.perf_counter()
+    print(
+        f"[========SUMMARY ran {passed + failed} tests ", end='')
+    if passed == 0:
+        print(f"{termcolor.colored(f'passed {passed}', 'magenta')} | ", end='')
+    else:
+        print(f"{termcolor.colored(f'passed {passed}', 'green')} | ", end='')
+    if failed == 0:
+        print(f"{termcolor.colored(f'failed {failed}', 'green')} | ", end='')
+    else:
+        if passed == 0:
+            print(f"{termcolor.colored(f'failed {failed}', 'magenta')} | ", end='')
+        else:
+            print(f"{termcolor.colored(f'failed {failed}', 'red')} | ", end='')
+
+    if failed == 0:
+        print(f"{termcolor.colored(f' 100%', 'green')} ", end='')
+    elif passed == 0:
+        print(f"{termcolor.colored(f' 0%', 'magenta')} ", end='')
+    elif passed / (failed + passed) <= 0.5:
+        print(f"{termcolor.colored(f' {round((passed / (failed + passed)) * 100, 2)}%', 'red')} ", end='')
+    else:
+        print(f"{termcolor.colored(f' {round((passed / (failed + passed)) * 100, 2)}%', 'yellow')} ", end='')
+    print(f"in {round(end_time - start_time, 2)} secs", end='')
+    print("========]")
