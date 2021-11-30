3340075efa312933ca38af8673f964f6835dc548
Alexandre <alexandre.josien@epita.fr>
2021-11-30 19:58:15 +0100

    cd files

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,22 +1,41 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
+#include <xstrdup.h>
+#include <stdlib.h>
 
 void cd(char **args)
 {
     if (args[1] == NULL)
         errx(1, "cd: Path is HOME");
 
-    char test_buffer[100];
+    char *path = xstrdup(args[1]); // on recup le path passe en arg
 
-    char *path;
-    strcpy(path, args[1]); // on recup le path passe en arg
+    int buffer_size = 1024;
+    char *current_path = calloc(1024, sizeof(char));
+    while (getcwd(current_path, buffer_size) == NULL)
+    {
+        buffer_size += 1024;
+        current_path = realloc(current_path, buffer_size);
+    }
+    current_path = getcwd(current_path, buffer_size);
 
     // print le current directory
-    printf("%s\n", getcwd(test_buffer, 100));
+    printf("You are ->%s\n", current_path);
+    printf("Go to -> %s\n\n", path);
     chdir(path);
-    printf(test_buffer, 100);
+    char *new_path = calloc(1024, sizeof(char));
+    new_path = getcwd(new_path, buffer_size);
+    if (current_path == new_path)
+        errx(1, "Pas possible to cd");
+
+    printf("You are now ->%s\n", new_path);
 }
 
 int main()
 {
-    printf("coucou\n");
+    char *arr[] = { "bite", "bite"};
+    printf("RUN TEST:\n\n");
+    cd(arr);
 }
