f8aff3699adb11c470950ae8b48c0faf0e1dee4a
Lucas Pluchon <lucas.pluchon@epita.fr>
2021-12-10 20:12:37 +0100

    Added some tests and finished dot

--- src/builtins/dot.c
+++ src/builtins/dot.c
@@ -4,55 +4,54 @@
 #include <stdio.h>
 #include <err.h>
 
+#include "read_script.h"
+#include "xparser.h"
+
 int contain_slash(char *str)
 {
     return strchr(str, '/') != NULL;
 }
 
-//char *create_path(char *path, char* file)
-//{
-//    char* res = calloc(strlen(path) + strlen(file) + 2, sizeof(char));
-//    if (path[strlen(path) - 1] == '/')
-//    {
-//        sprintf(res, "%s%s", path, file);
-//    }
-//    else
-//    {
-//        sprintf(res, "%s/%s", path, file);
-//    }
-//    return res;
-//}
-//
-//int dot(char **args)
-//{
-//    if (args[1] == NULL)
-//        return 0;
-//
-//    char *file = args[1];
-//    if (contain_slash(file))
-//    {
-//        char* folder = NULL;
-//        char* file_env = NULL;
-//        while ((folder = strtok(getenv("PATH"), ":")) != NULL)
-//        {
-//            file_env = create_path(folder, file);
-//            if(access(file_env, F_OK) == 0)
-//            {
-//                return 0;
-//            }
-//        }
-//    }
-//
-//    if(access(file, F_OK) != 0)
-//    {
-//        errx(2, "export: the maximum has been reached");
-//    }
-//
-//    return 0;
-//}
+char *create_path(char *path, char* file)
+{
+    char* res = calloc(strlen(path) + strlen(file) + 2, sizeof(char));
+    if (path[strlen(path) - 1] == '/')
+    {
+        sprintf(res, "%s%s", path, file);
+    }
+    else
+    {
+        sprintf(res, "%s/%s", path, file);
+    }
+    return res;
+}
 
 int dot(char **args)
 {
-    //TODO: Implement
-    return 0;
+    if (args[1] == NULL)
+        return 0;
+
+    char *file = args[1];
+    if (!contain_slash(file))
+    {
+        char* folder = NULL;
+        char* file_env = NULL;
+        while ((folder = strtok(getenv("PATH"), ":")) != NULL)
+        {
+            file_env = create_path(folder, file);
+            if(access(file_env, F_OK) == 0)
+            {
+                char *script = read_script(file_env);
+                return exec_script(script, strlen(file));
+            }
+        }
+    }
+
+    if(access(file, F_OK) != 0)
+    {
+        errx(2, "export: the maximum has been reached");
+    }
+
+    char *script = read_script(file);
+    return exec_script(script, strlen(file));;
 }
---
+++ tests/unit_tests/dot_test.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "builtins.h"
+
+//int main()
+//{
+//    char *arg0 = ".";
+//    char *arg1 = "/home/lucas/test.sh";
+//    char **args = calloc(3, sizeof(char*));
+//    args[0] = arg0;
+//    args[1] = arg1;
+//
+//    int test = dot(args);
+//    printf("%d\n", test);
+//
+//    free(args);
+//
+//    return 0;
+//}
