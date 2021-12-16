91cf012f1d63c038a276d2e78ef88e74205e96b2
Alex375 <alexandre.josien@gmail.com>
2021-12-16 18:36:58 +0100

    fixed cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <xstrdup.h>
@@ -7,50 +7,66 @@
 #include "vars.h"
 #include "xalloc.h"
 
-
-char *get_curdir(void) {
+char *get_curdir(void)
+{
     char *res = xcalloc(1, 4097);
     int size = 4096;
-    while (getcwd(res, 4096) == NULL) {
+    while (getcwd(res, 4096) == NULL)
+    {
         res = xrecalloc(res, size * 2 + 1);
         size *= 2;
     }
     return res;
 }
 
-
-int cd(char **args) {
+int cd(char **args)
+{
     char *path;
     char *oldpwd = getenv("PWD");
-    if (args[1] == NULL) {
+    if (args[1] == NULL)
+    {
         if (!getenv("HOME"))
             path = "/";
         else
             path = xstrdup(getenv("HOME"));
-        if (chdir(path) == -1) {
+        if (chdir(path) == -1)
+        {
             fprintf(stderr, "%s: No such file or directory\n", path);
             return 2;
-        } else {
+        }
+        else
+        {
             setenv("OLDPWD", oldpwd, 1);
             add_var("OLDPWD", oldpwd);
             setenv("PWD", path, 1);
         }
-    } else if (strcmp(args[1], "-") == 0) {
+    }
+    else if (strcmp(args[1], "-") == 0)
+    {
         char *target = getenv("OLDPWD");
-        if (chdir(target) == -1) {
+        if (chdir(target) == -1)
+        {
             fprintf(stderr, "%s: No such file or directory\n", target);
             return 2;
-        } else {
+        }
+        else
+        {
+            printf("%s\n", target);
             setenv("OLDPWD", oldpwd, 1);
             add_var("OLDPWD", oldpwd);
             setenv("PWD", target, 1);
         }
-    } else {
+    }
+    else
+    {
         path = xstrdup(args[1]);
-        if (chdir(path) == -1) {
+        if (chdir(path) == -1)
+        {
             fprintf(stderr, "%s: No such file or directory\n", path);
             return 2;
-        } else {
+        }
+        else
+        {
             setenv("OLDPWD", oldpwd, 1);
             add_var("OLDPWD", oldpwd);
             char *curdir = get_curdir();
