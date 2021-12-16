4d3cd225eeb796da2db72df2b6586403501e0c95
Alexandre <alexandre.josien@epita.fr>
2021-12-16 18:23:41 +0100

    added basic cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,122 +1,62 @@
-#include <err.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <xstrdup.h>
 
-#include "cd_vector.h"
 #include "vars.h"
+#include "xalloc.h"
 
-static struct vector *set_vector_PATH(char *path)
-{
-    struct vector *vec = vector_create();
-    char *token = strtok(path, "/");
-    vec->str = string_concat(vec->str, token);
-    while (token)
-    {
-        vector_add(vec, token);
-        token = strtok(NULL, "/");
+
+char *get_curdir(void) {
+    char *res = xcalloc(1, 4097);
+    int size = 4096;
+    while (getcwd(res, 4096) == NULL) {
+        res = xrecalloc(res, size * 2 + 1);
+        size *= 2;
     }
-
-    return vec;
+    return res;
 }
 
-static struct vector *set_vector_PWD()
-{
-    char *pwd = getenv("PWD");
-    struct vector *vec = vector_create();
-    char *token = strtok(pwd, "/");
-    while (token)
-    {
-        vector_add(vec, token);
-        token = strtok(NULL, "/");
-    }
 
-    return vec;
-}
-
-static int set_var(char *path)
-{
-    char *old = xstrdup(getenv("PWD"));
-    if (path[0] == '/')
-        setenv("PWD", "", 1);
-
-    struct vector *vector_PATH = set_vector_PATH(path);
-    struct vector *vector_PWD = set_vector_PWD();
-
-    for (struct vector *ptr = vector_PATH; ptr != NULL; ptr = ptr->next)
-    {
-        if (strcmp(ptr->str->data, "..") == 0)
-            vector_pop(vector_PWD);
-
-        else if (strcmp(ptr->str->data, ".") == 0)
-            continue;
-
-        else
-            vector_add(vector_PWD, vector_PATH->str->data);
-    }
-
-    struct string *value_PWD = string_create();
-    if (vector_PATH == NULL)
-        value_PWD = string_append(value_PWD, '/');
-
-    for (struct vector *ptr = vector_PWD; ptr != NULL; ptr = ptr->next)
-    {
-        value_PWD = string_append(value_PWD, '/');
-        value_PWD = string_concat(value_PWD, vector_PWD->str->data);
-    }
-
-    value_PWD = string_append(value_PWD, '\0');
-    setenv("OLDPWD", old, 1);
-    add_var("OLDPWD", old);
-    setenv("PWD", value_PWD->data, 1);
-
-    return 0;
-}
-
-static int minus()
-{
-    char *current = getenv("PWD");
-    char *old = getenv("OLDPWD");
-
-    int code = chdir(old);
-    if (code == -1)
-        errx(2, "Cannot obtain $OLDPWD");
-
-    char *tmp = xstrdup(current);
-    setenv("PWD", old, 1);
-    setenv("OLDPWD", tmp, 1);
-    add_var("OLDPWD", tmp);
-
-    printf("%s\n", getenv("PWD"));
-
-    free(tmp);
-    free(current);
-    free(old);
-
-    return 0;
-}
-
-int cd(char **args)
-{
+int cd(char **args) {
     char *path;
-    if (args[1] == NULL)
+    char *oldpwd = getenv("PWD");
+    if (args[1] == NULL) {
         if (!getenv("HOME"))
             path = "/";
         else
             path = xstrdup(getenv("HOME"));
-
-    else if (strcmp(args[1], "-") == 0)
-        return minus();
-
-    else
+        if (chdir(path) == -1) {
+            fprintf(stderr, "%s: No such file or directory\n", path);
+            return 2;
+        } else {
+            setenv("OLDPWD", oldpwd, 1);
+            add_var("OLDPWD", oldpwd);
+            setenv("PWD", path, 1);
+        }
+    } else if (strcmp(args[1], "-") == 0) {
+        char *target = getenv("OLDPWD");
+        if (chdir(target) == -1) {
+            fprintf(stderr, "%s: No such file or directory\n", target);
+            return 2;
+        } else {
+            setenv("OLDPWD", oldpwd, 1);
+            add_var("OLDPWD", oldpwd);
+            setenv("PWD", target, 1);
+        }
+    } else {
         path = xstrdup(args[1]);
-
-    int code = chdir(path);
-    if (code == -1)
-        errx(2, "%s: No such file or directory\n", path);
-
-    set_var(path);
+        if (chdir(path) == -1) {
+            fprintf(stderr, "%s: No such file or directory\n", path);
+            return 2;
+        } else {
+            setenv("OLDPWD", oldpwd, 1);
+            add_var("OLDPWD", oldpwd);
+            char *curdir = get_curdir();
+            setenv("PWD", curdir, 1);
+            xfree(curdir);
+        }
+    }
     return 0;
 }
